4b5cce7a35253f69c209cbd2bb47f6ff182327d9
Shubham <shubhmharbola07@gmail.com>
2026-05-18 17:10:27 +0530

    feat:connect mongoDB atlas by mongoose

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import connectDB from './config/database'
 dotenv.config()
 const app =  express()
 app.use(cors())
 app.use(express.json())
 
+connectDB()
 app.get('/',(req,res)=>{
     res.json("msg: smart lead api is running ")
 })
