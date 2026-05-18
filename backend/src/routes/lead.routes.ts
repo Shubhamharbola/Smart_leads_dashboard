24c4753d3454044366413dda1873d3ee94d45b3c
Shubham <shubhmharbola07@gmail.com>
2026-05-18 23:04:16 +0530

    feat:add role based acess control

--- backend/src/routes/lead.routes.ts
+++ backend/src/routes/lead.routes.ts
@@ -1,15 +1,14 @@
 import { Router } from 'express'
 import { createLead, getLeads, getLead, updateLead, deleteLead } from '../controllers/lead.controller'
 import { protect } from '../middleware/auth.middleware'
-
+import { authorizeRoles } from '../middleware/role.middleware'
 const router = Router()
 
 router.use(protect)
 
-router.post('/', createLead)
-router.get('/', getLeads)
-router.get('/:id', getLead)
-router.put('/:id', updateLead)
-router.delete('/:id', deleteLead)
-
+router.post('/', authorizeRoles('admin', 'sales'), createLead)
+router.get('/', authorizeRoles('admin', 'sales'), getLeads)
+router.get('/:id', authorizeRoles('admin', 'sales'), getLead)
+router.put('/:id', authorizeRoles('admin', 'sales'), updateLead)
+router.delete('/:id', authorizeRoles('admin'), deleteLead)
 export default router
