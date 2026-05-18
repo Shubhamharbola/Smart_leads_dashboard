f9fd9899fe7eb2d057dae3c7239b8a67f5a32a91
Shubham <shubhmharbola07@gmail.com>
2026-05-18 21:42:14 +0530

    add jwt auth with middelware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,6 +13,8 @@ app.use('/api/auth', authRoutes)
 app.get('/',(req,res)=>{
     res.json("msg: smart lead api is running ")
 })
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`)
---
+++ backend/src/middleware/auth.middleware.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express'
+import jwt from 'jsonwebtoken'
+
+export interface AuthRequest extends Request {
+  user?: {
+    id: string
+    role: string
+  }
+}
+
+export const protect = (req: AuthRequest, res: Response, next: NextFunction): void => {
+  try {
+    const token = req.headers.authorization?.split(' ')[1]
+
+    if (!token) {
+      res.status(401).json({ message: 'No token, authorization denied' })
+      return
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { id: string, role: string }
+    req.user = decoded
+    next()
+  } catch (error) {
+    res.status(401).json({ message: 'Token is not valid' })
+  }
+}
