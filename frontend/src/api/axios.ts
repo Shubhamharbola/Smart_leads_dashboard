249979ac3dc5e8a42621082843ea47af56c9eea0
Shubham <shubhmharbola07@gmail.com>
2026-05-19 11:18:30 +0530

    feat:change local host to render

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: ' https://smart-leads-dashboard-7y5m.onrender.com',
 })
 
 api.interceptors.request.use((config) => {
